f5ee8b58ea4370518698701924f8f4d030d8fb09
HimanshuP90 <himanshup895@gmail.com>
2018-02-23 23:57:58 +0530

    Added concepts

--- src/Chart.js
+++ src/Chart.js
@@ -5,12 +5,15 @@ import c3 from 'c3';
 export default class Chart extends React.Component {
 
   componentDidMount() {
+    console.log(ReactDOM.findDOMNode(this.refs.chart))
     this.chart = c3.generate({
       bindto: ReactDOM.findDOMNode(this.refs.chart),
       data: {
         columns: [
-          ['data1', 30, 200, 100, 400, 150, 250],
-          ['data2', 50, 20, 10, 40, 15, 25]
+          ['data1', 30, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25],
+          ['data3', 30, 400, 150, 250],
+          ['data4', 50, 20, 15, 25]
         ]
       }
     });
