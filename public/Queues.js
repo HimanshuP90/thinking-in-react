695f2702efa7754955e2625017bf857a58bbd61f
HimanshuP90 <himanshup895@gmail.com>
2018-03-04 22:56:03 +0530

    Queues concept added

---
+++ public/Queues.js
@@ -0,0 +1,31 @@
+//Queues
+
+function Queue() {
+	collection = [];
+	this.print = function() {
+		console.log(collection);
+	};
+
+	this.enqueue = function(element) {
+		collection.push(element);
+	};
+
+	this.dequeue = function() {
+		return collection.shift();
+	};
+
+	this.front = function() {
+		return collection[0];
+	};
+
+	this.size = function() {
+		return collection.length;
+	};
+
+	this.isEmpty = function() {
+		return (collection.length === 0);
+	};
+}
+
+var q = new Queue();
+console.log(q);
