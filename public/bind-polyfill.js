13872fb134cd037d0fb4bbac3e40ff3f9806b0a1
HimanshuP90 <himanshup895@gmail.com>
2018-11-28 22:26:24 +0530

    polyfill bind

---
+++ public/bind-polyfill.js
@@ -0,0 +1,36 @@
+Function.prototype.polyfillBind = function(oThis) {
+    if (typeof this !== 'function') {
+    throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+    }
+
+    var aArgs   = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP    = function() {},
+        fBound  = function() {
+        return fToBind.apply(this instanceof fNOP
+                ? this
+                : oThis,
+                aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    if (this.prototype) {
+    fNOP.prototype = this.prototype; 
+    }
+    fBound.prototype = new fNOP();
+
+    return fBound;
+};
+
+
+var mathLib = {
+	pi: 3.14,
+	area: function(r) {
+		return this.pi * r * r;
+	},
+	circumference: function(r) {
+		return 2 * this.pi * r;
+	}
+};
+
+var newContext = mathLib.circumference.polyfillBind({pi: 5})
+console.log(newContext(3))
